0db0bbf38dbf5db59ab0774d803b698a6d99ef1c
ABHISHEK SINGH <64076762+abk90133@users.noreply.github.com>
2020-06-16 17:47:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+//jsversion6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = ["Exercise", "Take Bath", "Make Breakfast"];
+
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("lists", {kindofday: day, newlistitems: items});
+
+
+});
+
+app.post("/", function(req, res){
+  var item = req.body.newitem;
+
+  items.push(item);
+
+  res.redirect("/");
+});
+
+
+
+app.listen(3201, function(){
+  console.log("Server started on port 3201");
+});
